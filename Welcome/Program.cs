a8d92513d6e6b4bf30bcc1c471d60d46cb88eb49
Gabriel <gabrieljones2001@outlook.com>
2022-05-10 09:50:08 -0700

    Intro C#
    just the first couple of assignments for the c# course

---
+++ Welcome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we\'re \"Acounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+
