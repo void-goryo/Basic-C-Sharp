b35c30f84640387eeb2cabb649bde7818fdd1dda
Gabriel <gabrieljones2001@outlook.com>
2022-05-11 15:35:22 -0700

    update

---
+++ VariablesAndDatatypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesAndDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is how you comment
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Your name is " + name);
+            //Console.ReadLine();
+            //Console.WriteLine("What is your favorite number?");
+            //string number = Console.ReadLine();
+            //int favnumber = Convert.ToInt32(number);
+            //int total = favnumber + 5;
+            //Console.WriteLine("Your favorite number plus 5 equals " + total);
+            //Console.ReadLine();
+        }
+    }
+}
