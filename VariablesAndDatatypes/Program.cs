232d2bfcf0402eb906171b8c799e1c3028af601b
Gabriel <gabrieljones2001@outlook.com>
2022-05-13 12:42:29 -0700

    Finished
    Finished Variables and Daily Report

--- VariablesAndDatatypes/Program.cs
+++ VariablesAndDatatypes/Program.cs
@@ -17,6 +17,29 @@ namespace VariablesAndDatatypes
             //int total = favnumber + 5;
             //Console.WriteLine("Your favorite number plus 5 equals " + total);
             //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            //char randomLetter = "A";
+            //char questionMark = "/u2103";
+            
+            decimal money = 100.5m;
+
+            double heightInCentimeters = 211.1239087541;
+
+            float secondsLeft = 2.62f;
+
+            short tempOnMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+            
         }
     }
 }
