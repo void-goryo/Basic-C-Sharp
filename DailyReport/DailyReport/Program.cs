e7471e33b02994b0ef873a34f802a97c06d48de5
Gabriel <gabrieljones2001@outlook.com>
2022-05-13 12:44:11 -0700

    forgot to add daily report

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Questions
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("What course are you on?");
+        string course = Console.ReadLine();
+        Console.WriteLine("What page number are you on?");
+        string page = Console.ReadLine();
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+        string help = Console.ReadLine();
+        Console.WriteLine("Were there any positive experiances you\'d like to share? Please give specifics.");
+        string exp = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you\'d like to provide? Please be specific.");
+        string feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        string hours = Console.ReadLine();
+
+        //response to entry
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.ReadLine();
+
+
+
+    }
+}
+
