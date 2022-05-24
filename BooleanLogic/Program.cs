8c2fa451f805632168c78b133d5857f22f95def3
Gabriel <gabrieljones2001@outlook.com>
2022-05-24 09:42:42 -0700

    finished booliean logic and insurance assignment

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && true);
+            Console.WriteLine(true && false);
+            Console.WriteLine(false && false);
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            //XOR will return true if one is false
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Insurance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //user information
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            //Print user information
+            Console.WriteLine("\n\n\n\n\nWhat is your age?");
+            Console.WriteLine(age);
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine(dui);
+            Console.WriteLine("How many speeding tickets do you have?");
+            Console.WriteLine(ticket);
+
+            //insurance check
+            if(age <= 15)
+            {
+                bool insurance = false;
+                Console.WriteLine("Qualified for insurance?");
+                Console.WriteLine(insurance);
+                Console.ReadLine();
+            }
+            else if(dui == true) {
+                bool insurance = false;
+                Console.WriteLine("Qualified for insurance?");
+                Console.WriteLine(insurance);
+                Console.ReadLine();
+            }
+            else if (ticket >= 3)
+            {
+                bool insurance = false;
+                Console.WriteLine("Qualified for insurance?");
+                Console.WriteLine(insurance);
+                Console.ReadLine();
+            }
+            else
+            {
+                bool insurance = true;
+                Console.WriteLine("Qualified for insurance?");
+                Console.WriteLine(insurance);
+                Console.ReadLine();
+            }
+        }
+    }
+}
