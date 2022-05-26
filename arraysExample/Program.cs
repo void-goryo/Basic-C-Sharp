9d8c4a027b18c8cc7e7b895dbbc5247638499b7a
Gabriel <gabrieljones2001@outlook.com>
2022-05-26 13:01:53 -0700

    array example

---
+++ arraysExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        //arrays
+        int[] numArray = new int[5]; //makes an array    [length]    also creates an object
+
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };    //does the same thing as the first array.
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000 };              //same thing as the first and second array.
+
+        numArray2[4] = 650;                                     //you can change the numbers of the array, but not the size.
+
+        //byte[] byteArray = new byte[5000];                      Used to store images
+
+
+        Console.WriteLine(numArray2[1]);
+        Console.WriteLine(numArray1[2]);
+        Console.WriteLine(numArray[3]);
+        Console.WriteLine(numArray2[4]);
+        Console.ReadLine();
+
+        //lists
+        List<int> intList = new List<int>();                    //you will use this more often. you can change size and adjust variables easier
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Gabe");
+        stringList.Add("Tyler");
+
+        Console.WriteLine(stringList[0]);
+        Console.ReadLine();
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+    }
+}
---
+++ stringAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intro = "Hello, ";
+            string name = "Gabriel!";
+            string sentence = " Welcome to the program!";
+
+            Console.WriteLine(intro + name + sentence);
+            Console.ReadLine();
+
+            name = name.ToUpper();
+
+            Console.WriteLine("Your name is now uppercase: " + name);
+            Console.ReadLine();
+
+            StringBuilder paragraph = new StringBuilder();
+
+            paragraph.Append("This is the first sentence");
+            paragraph.Append("\nThis is the second sentence");
+            paragraph.Append("\nThis is the last sentence");
+
+            Console.WriteLine("This is the StringBuilder in action!\n" + paragraph);
+            Console.ReadLine();
+        }
+    }
+}
