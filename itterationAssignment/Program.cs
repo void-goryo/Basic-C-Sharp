e140636a7f22464bae80f3f01b45abe34efc65de
Gabriel <gabrieljones2001@outlook.com>
2022-05-27 14:10:04 -0700

    not quite done yet

--- itterationAssignment/Program.cs
+++ itterationAssignment/Program.cs
@@ -1,12 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace itterationAssignment
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //creating string array
+        string[] stringArray = new string[] { "Tyler", "Emily", "Kaylee", "Celia" };
+        Console.WriteLine("Please enter your last name.");
+        string userString = Console.ReadLine();
+
+        //itterate through string array
+        for (int i = 0; i < stringArray.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            stringArray[i] = stringArray[i] + " " + userString;
         }
+
+        foreach (string name in stringArray)
+        {
+            Console.WriteLine(name);
+        }
+        Console.ReadLine();
+
+        //I'm honestly not sure what you're asking for on part two of the assignment, so this is my best take
+
+        //infinite loop
+        Console.WriteLine("SCP-2316");
+        Console.WriteLine("Do you recognize the bodies in the water? answer yes or no.");
+        string answer = Console.ReadLine().ToUpper();
+        bool bodies = false;
+        while (!bodies)
+        {
+            if(answer == "NO")
+            {
+                Console.WriteLine("Very Good.");
+                Console.WriteLine("Do you recognize the bodies in the water? answer yes or no.");
+                answer = Console.ReadLine().ToUpper();
+            }
+            else if (answer == "YES")
+            {
+                Console.Write("The Foundation told you that you don't recognize the bodies in the water. You are lost forever.");
+                bodies = true;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("You did not answer yes or no.");
+                Console.WriteLine("Do you recognize the bodies in the water? answer yes or no.");
+                answer = Console.ReadLine().ToUpper();
+            }
+        }
+        Console.ReadLine();
+
+        //more loops
+
+        for(int i = 0; i < 16; i++)
+        {
+            Console.WriteLine(i);
+        }
+        for(int i = 0; i <= 15; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        //list of strings
+        List<string> fruits = new List<string>() { "Apple", "Blueberry", "Coconut", "Duran", "Eggplant", "Fuji Apple", "Grape" };
+        Console.WriteLine("Please type a food that you would like to see.");
+        string food = Console.ReadLine().ToLower();
+        bool match = false;
+        do
+        {
+            switch (food)
+            {
+                case "apple":
+                    Console.WriteLine(fruits[0]);
+                    match = true;
+                    break;
+                case "blueberry":
+                    Console.WriteLine(fruits[1]);
+                    match = true;
+                    break;
+                case "coconut":
+                    Console.WriteLine(fruits[2]);
+                    match = true;
+                    break;
+                case "Duran":
+                    Console.WriteLine(fruits[3]);
+                    match = true;
+                    break;
+                case "eggplant":
+                    Console.WriteLine(fruits[4]);
+                    match = true;
+                    break;
+                case "fuji apple":
+                    Console.WriteLine(fruits[5]);
+                    match = true;
+                    break;
+                case "grape":
+                    Console.WriteLine(fruits[6]);
+                    match = true;
+                    break;
+                default:
+                    Console.Write("That food is not on the list. Please try again.\n");
+                    food = Console.ReadLine().ToLower();
+                    break;
+            }
+        }
+        while (!match);
+        Console.ReadLine();
+
+
+        List<string> stringList = new List<string>() { "Snake", "Snake", "Dog", "Cat" };
+        Console.WriteLine("Please select either a Dog, Cat, or Snake");
+        string animal = Console.ReadLine().ToLower();
+        match = false;
+
+        
     }
 }
