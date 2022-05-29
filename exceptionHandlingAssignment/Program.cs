7ee795e80d8958697dde5283639b2b4f928c4feb
Gabriel <gabrieljones2001@outlook.com>
2022-05-28 17:33:14 -0700

    finished

--- exceptionHandlingAssignment/Program.cs
+++ exceptionHandlingAssignment/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace exceptionHandlingAssignment
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> divide = new List<int>() { 20, 30, 40, 50, 60 };
+
+        try
         {
-            Console.WriteLine("Hello World!");
+            //user input
+            Console.WriteLine("Please input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //logic
+            for (int i = 0; i < divide.Count; i++)
+            {
+                Console.WriteLine(number + " / " + divide[i] + " = " + divide[i] / number);
+            }
+        }
+        //catches 0's
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        //catches strings
+        catch(FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        //runs no mater what
+        finally
+        {
+            Console.ReadLine();
         }
     }
 }
