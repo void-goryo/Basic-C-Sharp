0e28a035becf32a1de3f10fb2e7fe184ed08f10c
Gabriel <gabrieljones2001@outlook.com>
2022-06-02 12:54:44 -0700

    blackjack
    finished a couple of classes and made a random method

--- blackjack/Deck.cs
+++ blackjack/Deck.cs
@@ -10,12 +10,27 @@ namespace blackjack
     {
         public Deck()
         {
-            Cards = new List<Card>();
-            Card cardOne = new Card();
-            cardOne.Face = "Two";
-            cardOne.Suit = "Hearts";
-            Cards.Add(cardOne);
+            Cards = new List<Card>();   //Card is a model being taken from the class "Cards", which houses a face and suit that can hold a value set to it
+
+            //makes two lists of values
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three","Four" ,"Five","Six","Seven","Eight", "Nine", "Ten", "Ace", "Jack", "Queen", "King"
+            };
+
+            foreach(string face in Faces)
+            {
+                foreach(string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
         }
-        public List<Card> Cards { get; set; }
+        public List<Card> Cards { get; set; }   //this creates the list cards, which allows the top list to be created. This allows us to either set a value, or 
+                                                //get a value from another class
     }
 }
--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -12,11 +12,32 @@ namespace blackjack
         {
 
             Deck deck = new Deck();
+            deck = Shuffle(deck);
             
 
+            foreach(Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
 
             Console.WriteLine(deck.Cards[0].Face + " of " + deck.Cards[0].Suit);
             Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> tempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                tempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = tempList;
+            return deck;
         }
     }
 }
