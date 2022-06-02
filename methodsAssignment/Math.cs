ac08db7cee92d35db23bc34e7d4843f8480463a7
Gabriel <gabrieljones2001@outlook.com>
2022-06-02 14:33:03 -0700

    I'm stuck

---
+++ methodsAssignment/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodsAssignment
+{
+    public class Math
+    {
+        public Math Addition()
+        {
+            int addedNum = number + 5;
+            return addedNum;
+        }
+        public int number { get; set; } //sets a variable to a public variable that can be called and set in other classes
+    }
+}
--- methodsAssignment/Program.cs
+++ methodsAssignment/Program.cs
@@ -6,7 +6,9 @@ namespace methodsAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What number would you like to preform math oppreations on?");
+            Math userNum = new Math();
+            userNum.number = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
