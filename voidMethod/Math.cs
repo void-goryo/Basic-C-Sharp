f534aa06ac79e00d0a3567a55ebbcd34d3d6485f
Gabriel <gabrieljones2001@outlook.com>
2022-06-06 11:17:35 -0700

    finished

---
+++ voidMethod/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace voidMethod
+{
+    class Math
+    {
+        public void addition(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ voidMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace voidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math user = new Math();
+            Console.WriteLine("Please enter two numbers.");
+            Console.WriteLine("First number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            user.addition(num3, num4);
+        }
+    }
+}
