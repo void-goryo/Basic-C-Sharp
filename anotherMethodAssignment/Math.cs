ec6a88e502f383a0337c520596a7f62a5fd76e1c
Gabriel <gabrieljones2001@outlook.com>
2022-06-06 10:48:38 -0700

    done

---
+++ anotherMethodAssignment/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace anotherMethodAssignment
+{
+    class Math
+    {
+        public int CompAdd(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+    }
+}
--- anotherMethodAssignment/Program.cs
+++ anotherMethodAssignment/Program.cs
@@ -6,7 +6,15 @@ namespace anotherMethodAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Math user = new Math();
+            Console.WriteLine("Please enter two numbers. Note: you do not have to enter a second number");
+            Console.WriteLine("Number one:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number two:");
+            int? num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(num4);
+            user.CompAdd(num3, num5);
+
+         }
+     }
 }
