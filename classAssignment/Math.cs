0f02eaef75ac70686dc0b59e391a42cbf10801af
Gabriel <gabrieljones2001@outlook.com>
2022-06-06 13:26:31 -0700

    completed assignment and updated blackjack

---
+++ classAssignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classAssignment
+{
+    //This is how you would make it so you cannot instantiate the class
+    static class Math
+    {
+        public static void Process(int user)
+        {
+            int answer = user * 8;
+            Console.WriteLine(answer);
+        }
+
+        public static void Process(int user, int number)
+        {
+            int answer = user / number;
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ classAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace classAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to process");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //You cannot instantiate the class due to it being a static class. This is how you would call the class and the methods inside of it.
+            Math.Process(input);
+            Math.Process(input, 5);
+        }
+    }
+}
