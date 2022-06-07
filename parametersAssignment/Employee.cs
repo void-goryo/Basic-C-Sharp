b6763ffbb4d5e4823aa15a48b6d3b1878ffee3fe
Gabriel <gabrieljones2001@outlook.com>
2022-06-07 13:46:31 -0700

    finished assignment

---
+++ parametersAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parametersAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> things { get; set; }
+
+
+
+    }
+}
---
+++ parametersAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace parametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> user1 = new Employee<string>();
+            Employee<int> user2 = new Employee<int>();
+            user1.things = new List<string>() { "Gabe", "Tyler" };
+            user2.things = new List<int>() { 5, 10 };
+
+
+            foreach (string i in user1.things)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            foreach (int i in user2.things)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
