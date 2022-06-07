68b48e4b6bf8e38018c549ce829ed734a7590e06
Gabriel <gabrieljones2001@outlook.com>
2022-06-07 10:17:31 -0700

    blackjack update

--- blackjack/Game.cs
+++ blackjack/Game.cs
@@ -6,7 +6,7 @@ namespace blackjack
 {
     public abstract class Game
     {
-        public List<string> Players { get; set; }
+        public List<Player> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
 
@@ -14,9 +14,9 @@ namespace blackjack
 
         public virtual void ListPlayers()       //a virtual method in an abstract class means that the method gets inhereted in an inhereting class to be overwriten
         {
-            foreach (string player in Players)
+            foreach (Player player in Players)
             {
-                Console.WriteLine(player);
+                Console.WriteLine(player.Name);
             }
         }
     }
--- blackjack/Program.cs
+++ blackjack/Program.cs
@@ -10,22 +10,24 @@ namespace blackjack
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Gabe", "Bill", "Joe" };
-            game.ListPlayers();
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "game";
+            game = game + player;
 
 
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
 
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
