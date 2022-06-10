5092f3f01f6076827097653ec54a54272323779c
Gabriel <gabrieljones2001@outlook.com>
2022-06-10 01:29:07 -0700

    Constructor assignment done

--- constructorAssignment/Program.cs
+++ constructorAssignment/Program.cs
@@ -4,9 +4,19 @@ namespace constructorAssignment
 {
     class Program
     {
+        public Program(string name) : this(name, 100) { }       //default
+        public Program(string name, int value = 0)      //constructer that would run after the "default values"
+        {
+            //pretend this is code. I did this it 1:30am and don't really know what to put in here rn for this assignment lol
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string hi = "hello?";     //will never change
+
+            var num = 10;                   //a number that implicitly knows that it's an int
+
         }
     }
 }
