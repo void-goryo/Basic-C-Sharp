bc76a5fe16710e776fb5d1e4ca9c190cfd70a2fb
Gabriel <gabrieljones2001@outlook.com>
2022-06-10 04:26:43 -0700

    done with assignment

--- trycatchAssignment/Age.cs
+++ trycatchAssignment/Age.cs
@@ -8,11 +8,16 @@ namespace trycatchAssignment
     {
         public int wasBorn(int age)
         {
-            DateTime date = new DateTime();
-            date = DateTime.Now;
-            int year = date.Year;
+            DateTime date = new DateTime();     //starts a new GetTime
+            date = DateTime.Now;                //gets current time
+            int year = date.Year;               //grabs year from current time and stores it in a new int
 
-            return year - age;
+            if(age <= 0)        //exception if user puts in anything under 0
+            {
+                throw new Exception();
+            }
+
+            return year - age;  //does math and returns the answer
 
         }
     }
--- trycatchAssignment/Program.cs
+++ trycatchAssignment/Program.cs
@@ -8,10 +8,17 @@ namespace trycatchAssignment
         {
             Age year = new Age();
             Console.Write("What is your age? ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(year.wasBorn(age));
-
+            try
+            {
+                Console.WriteLine(year.wasBorn(age));
+            }
+            catch (Exception)       //this runs when an exception has been thrown
+            {
+                Console.WriteLine("You have not been born yet.");
+            }
+            Console.ReadLine();
         }
     }
 }
